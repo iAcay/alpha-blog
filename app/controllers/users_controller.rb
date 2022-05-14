6aa8049386a59393fd820653f3c2611c444987d7
iAcay <96024046+iAcay@users.noreply.github.com>
2022-05-14 17:26:45 +0200

    add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to articles_path, notice: "Welcome #{@user.username} in our community. We're glad you here!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
