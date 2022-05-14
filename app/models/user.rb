81886c9db77ac8616593954609b970212e43db66
iAcay <96024046+iAcay@users.noreply.github.com>
2022-05-14 17:05:01 +0200

    feat: add user mvc and generalize forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles
   validates :username, presence: true, 
                       uniqueness: { case_sensitive: false }, 
@@ -6,4 +7,6 @@ class User < ApplicationRecord
   validates :email, presence: true, 
                   uniqueness: { case_sensitive: false }, 
                   length: { maximum: 105 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  
+  has_secure_password
 end
