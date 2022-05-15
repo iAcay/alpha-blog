8f72d1d0dcdde25f0fd5894a90530a8156d33b5d
iAcay <96024046+iAcay@users.noreply.github.com>
2022-05-15 13:35:21 +0200

    feat: user edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to articles_path, notice: "Your account was successfully updated."
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
